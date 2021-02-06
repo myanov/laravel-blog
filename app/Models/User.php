06a7c6031bf648238ad8bb753d3d976c032729a0
Maksim <m.yanov1998@gmail.com>
2021-02-06 17:58:13 +0700

    Added user model for admin part

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,17 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+
+    const IS_ADMIN = 1;
+    const IS_NORMAL = 0;
+    const BANNED = 1;
+    const UN_BANNED = 0;
 
     /**
      * The attributes that are mass assignable.
@@ -19,7 +26,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
     ];
 
     /**
@@ -49,5 +55,99 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public static function add($fields)
+    {
+        $user = new static;
+        $user->fill($fields);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function generatePassword($password)
+    {
+        if($password != null) {
+            $this->password = bcrypt($password);
+            $this->save();
+        }
+    }
+
+    public function remove()
+    {
+        $this->remove();
+    }
+
+    public function uploadAvatar($image)
+    {
+        if($image == null) {return;}
+
+        $this->removeAvatar();
+
+        $filename = Str::random(10) . '.' . $image->extension();
+        $image->storeAs('uploads', $filename);
+        $this->avatar = $filename;
+        $this->save();
+    }
+
+    public function removeAvatar()
+    {
+        if($this->avatar != null)
+            Storage::delete('uploads/' . $this->avatar);
+    }
+
+    public function getAvatar()
+    {
+        if($this->avatar == null)
+            return '/img/no-image.png';
+
+        return '/uploads/' . $this->avatar;
+    }
+
+    public function makeAdmin()
+    {
+        $this->is_admin = User::IS_ADMIN;
+        $this->save();
+    }
+
+    public function makeNormal()
+    {
+        $this->is_admin = User::IS_NORMAL;
+        $this->save();
+    }
+
+    public function toggleAdmin($value)
+    {
+        if($value == null)
+            return $this->makeNormal();
+
+        return $this->makeAdmin();
+    }
+
+    public function ban()
+    {
+        $this->status = User::BANNED;
+        $this->save();
+    }
+
+    public function unBan()
+    {
+        $this->status = User::UN_BANNED;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if($value == null)
+            return $this->unBan();
+
+        return $this->ban();
     }
 }
